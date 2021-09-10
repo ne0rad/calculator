5d4556650cbb66ab0a01da1dec9adf717dea167e
ne0rad <usackasr@gmail.com>
2021-09-10 14:04:49 +0100

    Added display html/css

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function add() {
+    args = [...arguments];
+    return args[0] + args[1];
+}
+
+function substract() {
+    args = [...arguments];
+    return args[0] - args[1];
+}
+
+function divide() {
+    args = [...arguments];
+    return args[0] / args[1];
+}
+
+function multiply() {
+    args = [...arguments];
+    return args[0] * args[1];
+}
