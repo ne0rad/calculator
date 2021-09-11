683f6908d0b8a96b33f64177ae17b1bc40475f21
ne0rad <usackasr@gmail.com>
2021-09-11 13:30:43 +0100

    Numbers, C, Del functionality

--- js/script.js
+++ js/script.js
@@ -17,3 +17,19 @@ function multiply() {
     args = [...arguments];
     return args[0] * args[1];
 }
+
+function numberClick(num) {
+    let bottom = document.getElementById("bottom");
+    if(bottom.innerHTML == 0) bottom.innerHTML = num.toString();
+    else bottom.innerHTML = bottom.innerHTML + num.toString();
+}
+
+function clearEverything() {
+    document.getElementById("bottom").innerHTML = 0;
+}
+
+function backspace() {
+    let bottom = document.getElementById("bottom");
+    if(bottom.innerHTML.length <= 1) bottom.innerHTML = 0;
+    else bottom.innerHTML = bottom.innerHTML.substring(0, bottom.innerHTML.length - 1);
+}
