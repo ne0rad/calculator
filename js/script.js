57dfafb7eaeccd9fd97c339cb0ffce214e29f308
ne0rad <usackasr@gmail.com>
2021-09-12 12:31:03 +0100

    Equals button functionality, operators

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-function add() {
-    args = [...arguments];
-    return args[0] + args[1];
-}
-
-function substract() {
-    args = [...arguments];
-    return args[0] - args[1];
-}
-
-function divide() {
-    args = [...arguments];
-    return args[0] / args[1];
-}
-
-function multiply() {
-    args = [...arguments];
-    return args[0] * args[1];
-}
-
 function numberClick(num) {
     let bottom = document.getElementById("bottom");
     if(bottom.innerHTML == 0) bottom.innerHTML = num.toString();
@@ -26,10 +6,45 @@ function numberClick(num) {
 
 function clearClick() {
     document.getElementById("bottom").innerHTML = 0;
+    document.getElementById("top").innerHTML = "&nbsp;";
 }
 
 function delClick() {
     let bottom = document.getElementById("bottom");
     if(bottom.innerHTML.length <= 1) bottom.innerHTML = 0;
     else bottom.innerHTML = bottom.innerHTML.substring(0, bottom.innerHTML.length - 1);
+}
+
+function operatorClick(operator) {
+    let bottom = document.getElementById("bottom");
+    let top = document.getElementById("top");
+    top.innerHTML = bottom.innerHTML + operator;
+    bottom.innerHTML = 0;
+}
+
+function equalsClick() {
+    let bottom = document.getElementById("bottom");
+    let top = document.getElementById("top");
+    let topNum = parseInt(top.innerHTML.slice(0,top.innerHTML.length-1));
+    let botNum = parseInt(bottom.innerHTML);
+    switch (top.innerHTML[top.innerHTML.length-1]) {
+        case "+":
+            bottom.innerHTML = topNum + botNum;
+            top.innerHTML += botNum;
+            break;
+        case "-":
+            bottom.innerHTML = topNum - botNum;
+            top.innerHTML += botNum;
+            break;
+        case "÷":
+            bottom.innerHTML = topNum / botNum;
+            top.innerHTML += botNum;
+            break;
+        case "×":
+            bottom.innerHTML = topNum * botNum;
+            top.innerHTML += botNum;
+            break;
+        default:
+            break;
+    }   
 }
